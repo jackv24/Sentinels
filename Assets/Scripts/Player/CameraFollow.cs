08d99dbca4104028b89f8cbd3be0a43001f27af3
skivix <furyismad@gmail.com>
2014-11-30 20:06:41 +1100

    Camerafollow added

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform Target;
+	public float speed = 10.0f;
+	
+	Vector3 direction;
+	
+	void Update() {
+		direction = Target.position - transform.position;
+		direction.y = 0;
+		float distance = direction.magnitude;
+		direction.Normalize();
+		
+		if(distance > 2){			
+			transform.position = transform.position + direction * speed * Time.deltaTime;
+		}
+	}
+	
+	
+	
+	
+}
