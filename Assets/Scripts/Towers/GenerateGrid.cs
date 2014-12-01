8719670d033aa307264f7f3ffeaeba372985431c
jackv24 <jackv_24@yahoo.com.au>
2014-12-01 22:50:08 +1030

    Each node now has it's own script containing properties. Turrets can now be placed on empty nodes.

---
+++ Assets/Scripts/Towers/GenerateGrid.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateGrid : MonoBehaviour
+{
+    //The node prefab to be instantiated in a grid
+    public GameObject nodePrefab;
+    //The size of the grid (in node amounts)
+    public Vector2 size;
+    //The layer for rays to be cast to determine node height
+    public LayerMask layer;
+
+    //Should a grid be generated?
+    public bool generate = false;
+
+    void Start()
+    {
+        //If a grid is to be generated...
+        if (generate)
+        {
+            //Get the size of the node's box collider
+            float sizeX = nodePrefab.transform.localScale.x;
+            float sizeY = nodePrefab.transform.localScale.z;
+
+            //Create the grid. For every row...
+            for (int i = 0; i < size.x; i++)
+            {
+                //Generate a column
+                for (int j = 0; j < size.y; j++)
+                {
+                    //Create the vector at which the node should be placed
+                    Vector3 placeVector = new Vector3(i * sizeX + transform.position.x, 500, j * sizeY + transform.position.z);
+
+                    RaycastHit hitInfo;
+                    //Cast a ray down onto layer
+                    if (Physics.Raycast(placeVector, -Vector3.up, out hitInfo, 1000, layer))
+                    {
+                        //Placement height is where ray hit
+                        placeVector.y = hitInfo.point.y;
+
+                        //Instantiate node
+                        GameObject obj = (GameObject)Instantiate(nodePrefab, placeVector, Quaternion.identity);
+                        //Set node as child of grid
+                        obj.transform.parent = transform;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Towers/PlaceTowers.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceTowers : MonoBehaviour
+{
+    //The layer on which towers can be placed
+    public LayerMask layer;
+
+    //The tower prefab to instantiate
+    public GameObject towerPrefab;
+    //The current tower, to follow the mouse cursor over the grid for placement
+    private GameObject currentTower;
+
+    void Start()
+    {
+        //Spawns a single tower, and sets it as the current (for testing)
+        currentTower = (GameObject)Instantiate(towerPrefab, Vector3.up * 100, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        //Ray to be cast from the mouse outward
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 100f, layer))
+        {
+            GridNode node = hitInfo.collider.gameObject.GetComponent<GridNode>();
+
+            //If the node is not occupied...
+            if (!node.isOccupied)
+            {
+                //Set the position of the current tower to the grid tile position
+                currentTower.transform.position = hitInfo.transform.position;
+
+                //When the player clicks the left mouse button...
+                if (Input.GetMouseButton(0))
+                {
+                    //Set the node to occupied
+                    node.isOccupied = true;
+                    //Place the tower
+                    PlaceTower(towerPrefab, hitInfo.transform.position);
+
+                    //Move the current tower out of view
+                    currentTower.transform.position = Vector3.up * 100;
+                }
+            }
+        }
+    }
+
+    void PlaceTower(GameObject towerPrefab, Vector3 position)
+    {
+        GameObject tower = (GameObject)Instantiate(towerPrefab, position, Quaternion.identity);
+
+        GameObject towers;
+
+        if (!GameObject.Find("Towers"))
+            towers = new GameObject("Towers");
+        else
+            towers = GameObject.Find("Towers");
+
+        tower.transform.parent = towers.transform;
+    }
+}
