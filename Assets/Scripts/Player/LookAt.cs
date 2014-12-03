188d4df1e5c8e56c032994d29c0fb52de7a69195
skivix <furyismad@gmail.com>
2014-12-03 12:59:10 +1100

    Added Player/LookAt.cs

---
+++ Assets/Scripts/Player/LookAt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+	
+	// Use this for initializationvoid Start () { 
+	
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 mouse = Input.mousePosition;
+		
+		//  Cast a ray from the screen into the world
+		Ray ray = Camera.main.ScreenPointToRay(mouse);
+		RaycastHit hitInfo;
+		Physics.Raycast(ray, out hitInfo);
+		
+		//  Adjust target position's Y
+		Vector3 target = hitInfo.point;
+		target.y = transform.position.y;
+		transform.LookAt(target);
+		
+	}
+}
