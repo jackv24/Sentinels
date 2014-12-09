9fc399228c9d147ec03658f5015013dcb751e1d1
jackv24 <jackv_24@yahoo.com.au>
2014-12-09 18:07:37 +1030

    Main Menu level select is now fully functional.

---
+++ Assets/Scripts/UI/UIBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIBehaviour : MonoBehaviour
+{
+    public GameObject[] panels;
+
+    void Start()
+    {
+        ClosePanels();
+    }
+
+    public void OpenPanel(int index)
+    {
+        ClosePanels();
+
+        panels[index].SetActive(true);
+    }
+
+    public void ClosePanels()
+    {
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    public void LoadLevel(int index)
+    {
+        Application.LoadLevel(index);
+    }
+
+    public void LoadLevel(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
