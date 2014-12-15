b94364b7c99ffc59b75df242d2be1816eebce847
PaulLeitch <pleitch92@gmail.com>
2014-12-15 11:41:58 +1100

    Changes to Pauls_Test_Scene

---
+++ Assets/Scenes/Testing/Test_Scripts/EnemyNavMesh.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNavMesh : MonoBehaviour 
+{
+
+	NavMeshAgent navMeshAgent;
+	public GameObject Target;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		navMeshAgent.SetDestination (Target.transform.position);
+	}
+}
