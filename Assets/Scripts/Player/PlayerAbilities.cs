55ae7f801dd456d40a1dcbbe97824d5cf6327609
skivix <furyismad@gmail.com>
2014-12-18 22:44:41 +1100

    Abilites started map changed

---
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAbilities : MonoBehaviour {
+	public Abilities ability;
+		public enum Abilities
+	{
+		NormalShoot,
+		MultiShot,
+		Blink,
+		FreezingFire,
+		RapidFire,
+		SelfRepair,
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  switch(ability)
+		{
+		case Abilities.NormalShoot: 
+				break;
+		case Abilities.MultiShot:
+			break;
+
+		case Abilities.Blink:
+			break;
+
+		case Abilities.FreezingFire:
+			break;
+
+		case Abilities.RapidFire:
+			break;
+
+		case Abilities.SelfRepair:
+			break;
+		}
+		
+	}
+}
