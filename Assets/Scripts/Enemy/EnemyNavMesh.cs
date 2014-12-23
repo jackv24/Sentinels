797a55ecc86f469013206229a65f58581f71de5e
PaulLeitch <pleitch92@gmail.com>
2014-12-24 00:55:59 +1100

    ARE YOU HAPPY NOW!

---
+++ Assets/Scripts/Enemy/EnemyNavMesh.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNavMesh : MonoBehaviour 
+{
+	NavMeshAgent navMeshAgent;
+	Transform Target;
+		
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Target = GameObject.FindGameObjectWithTag ("Finish").transform;
+		navMeshAgent = GetComponent <NavMeshAgent> ();
+		navMeshAgent.SetDestination (Target.transform.position);
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+	public GameObject Enemy;
+    public float EnemySpawn = 100f;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		EnemySpawn -= 1;
+
+        if (EnemySpawn < 10)
+        {
+            float RandNum = Random.Range(0, 4);
+            float ChosenNum = RandNum;
+
+            if (ChosenNum <= 2)
+            {
+                GameObject Spawn = Instantiate(Enemy, transform.position, transform.rotation) as GameObject;
+            }
+
+            EnemySpawn = 100f;
+        }
+	}
+}
