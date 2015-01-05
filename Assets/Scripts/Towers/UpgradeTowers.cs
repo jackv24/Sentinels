ba2b81580d72c27380a40b0364cf3bb1556259dc
Jack Vine <jackv_24@yahoo.com.au>
2015-01-05 11:11:59 +1100

    Moved tower images into resources folder. Also finished the graphical portion of tower upgrading.

---
+++ Assets/Scripts/Towers/UpgradeTowers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpgradeTowers : MonoBehaviour
+{
+    public Text nameText;
+
+    public Image towerImage;
+    private Sprite towerSprite;
+
+    public Slider healthSlider;
+    public Slider xpSlider;
+    public Slider levelSlider;
+
+    public Text nextLevelText;
+
+    void Start()
+    {
+        towerSprite = Resources.Load<Sprite>("Sprites/Turret");
+
+        towerImage.sprite = towerSprite;
+    }
+}
